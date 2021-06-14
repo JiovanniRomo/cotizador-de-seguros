9353310ade77d3cca8d14aac64c31dde8c5b4831
Jiovanni Romo (W) <srr190802@gmail.com>
2021-06-13 21:16:48 -0500

    Se redujeron los espacios innecesarios en el metodo calcularSeguro

--- js/index.js
+++ js/index.js
@@ -94,38 +94,28 @@ class Seguro {
     }
 
     calcularSeguro() {
-
         let cantidad;
         const base = 2000;
         switch (this.marca) {
-
             case '1':
                 cantidad = base * 1.15;
                 break;
-
             case '2':
                 cantidad = base * 1.05;
                 break;
-
             case '3':
                 cantidad = base * 1.35;
                 break;
-
             default:
                 break;
         }
-
         const diferencia = new Date().getFullYear - this.year;
-
         cantidad -= ((diferencia * 3) * cantidad) / 100;
-
         if (this.tipo === 'basico') {
             cantidad *= 1.30;
         } else {
             cantidad *= 1.50;
         }
-
-        console.log(cantidad)
         return cantidad;
     }
 }
